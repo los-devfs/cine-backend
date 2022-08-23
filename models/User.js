ba348380bbcd9282624d8929cfcfdd02113d55f9
kriss <sdckrissminano@gmail.com>
2022-08-22 21:36:07 -0500

    create model user

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  name: {
+    type:String,
+    required:true
+  },
+  lastName: {
+    type:String,
+    required:true
+  },
+  role:  {
+    type:String,
+    required:true
+  },
+  email:  {
+    type:String,
+    required:true
+  },
+  password:  {
+    type:String,
+    required:true
+  },
+  birthDate: Date,
+});
+
+export default mongoose.model("User", schema);

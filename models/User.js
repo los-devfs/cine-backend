b26715867ecdfd596387042779c9d9f63e352c32
kriss <sdckrissminano@gmail.com>
2022-08-22 21:54:35 -0500

    enum rol del user

--- models/User.js
+++ models/User.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: {
-    type:String,
-    required:true
+  name:{
+    type: String,
+    required: true,
   },
-  lastName: {
-    type:String,
-    required:true
+  lastName:{
+    type: String,
+    required: true,
   },
-  role:  {
-    type:String,
-    required:true
+  role:{
+    type: String,
+    enum : ['Administrator','Employee','Customer'],
+    default: 'Employee',
+    required: true,
+    
   },
-  email:  {
-    type:String,
-    required:true
+  email:{
+    type: String,
+    required: true,
   },
-  password:  {
-    type:String,
-    required:true
+  password:{
+    type: String,
+    required: true,
   },
   birthDate: Date,
 });

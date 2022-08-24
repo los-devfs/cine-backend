1875642df3fa38695648d11c5c2975be5d1b9f05
Patricia Ballardo <patriciaballardoc@gmail.com>
2022-08-23 21:00:57 -0500

    envio de correo para verificar cuenta

--- api/config/index.js
+++ api/config/index.js
@@ -9,4 +9,13 @@ export default {
   database: {
     uri: process.env.URI || 'mongodb://localhost/cine',
   },
+  email:{
+      host:process.env.EMAIL_HOST,
+      port:process.env.EMAIL_PORT,
+      auth:{
+          user:process.env.EMAIL_AUTH_USER,
+          pass:process.env.EMAIL_AUTH_PASS,
+      },
+      from:process.env.EMAIL_FROM,
+  }
 };

67024d1288cb51c243ffb5bf0d371887ffe59a87
TefyCastVent <94312111+TefyCastVent@users.noreply.github.com>
2022-08-24 19:08:38 -0500

    modification of the User file making email unique

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+  },
+  lastName:{
+    type: String,
+    required: true,
+  },
+  role:{
+    type: String,
+    enum : ['Administrator','Employee','Customer'],
+    default: 'Employee',
+    required: true,
+    
+  },
+  email:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password:{
+    type: String,
+    required: true,
+  },
+  birthDate: Date,
+});
+
+export default mongoose.model("User", schema);

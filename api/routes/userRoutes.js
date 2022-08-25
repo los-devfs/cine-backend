999c98671bd736a1341c1e68856b513f14d421e4
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-08-24 20:55:45 -0500

    fix: remove register part in useRouter

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,13 +1,8 @@
 import express from "express";
-import User from "../models/User.js";
 import updateUserValidator from '../middlewares/updateUserValidator.js';
-import createUserValidator from "../middlewares/createUserValidator.js";
 import * as userController from '../controllers/userController.js';
-import { register } from "../controllers/authController.js";
 
 const router = express.Router();
-
-router.post('/createUser', createUserValidator, register)
 
 router.put('/desableUser/:id', userController.disableUserbyId);
 

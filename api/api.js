51cebe30a2b42fcd93528dd707978f246372fbaf
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-08-24 21:19:38 -0500

    resolved the api.js problem

--- api/api.js
+++ api/api.js
@@ -1,19 +1,25 @@
 import express from 'express';
-import userRouter from './routes/userRoutes.js';
-import movieRoutes from './routes/movieRoutes.js'
+import projectionRoutes from './routes/projectionRoutes.js';
+import movieRoutes from './routes/movieRoutes.js';
+import roomRoutes from './routes/roomRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const api = express();
-api.use(express.json());
 
 //TODO: registrar middlewares y rutas
+api.use(express.json());
 
-api.get("/status", (_, res) => {
-  retun;
+api.get('/status', (_, res) => {
+  return res.json({
+    msg: 'API funcionando',
+  });
 });
 
-
-// aca se registran las rutas
-api.use(userRouter);
+api.use(userRoutes);
 api.use(movieRoutes);
+api.use(roomRoutes);
+
+//registrar ruta de projection:
+api.use('/projections', projectionRoutes);
 
 export default api;

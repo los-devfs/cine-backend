230db12d3f10655b50ab568cab976a2fcb48e7dc
emanuelretamozo <eretamozov@gmail.com>
2022-08-24 20:30:56 -0500

    fix: updating middleware of updatingMovie 3

--- api/middlewares/createMovieValidator.js
+++ api/middlewares/createMovieValidator.js
@@ -14,7 +14,7 @@ const genreSchema = joi.object().keys({
     ),
 });
 
-const createMovieSchema = joi.object({
+const movieBodyValidatorSchema = joi.object({
   title: joi.string().required(),
   director: joi.string().required(),
   producer: joi.string().required(),
@@ -26,22 +26,11 @@ const createMovieSchema = joi.object({
   synopsis: joi.string()
 });
 
-const updateMovieSchema = joi.object({
-  title: joi.string(),
-  director: joi.string(),
-  producer: joi.string(),
-  cover: joi.string(),
-  releaseDate: joi.date(),
-  rated: joi.number().integer().positive().min(1).max(5),
-  genre: joi.string(),
-  cast: joi.array().items(joi.string()),
-  synopsis: joi.string()
-});
 
 export default async (req, res, next) => {
   try {
     console.log(req.body);
-    await createMovieSchema.validateAsync(req.body);
+    await movieBodyValidatorSchema.validateAsync(req.body);
     next();
   } catch (error) {
     return res.status(400).json({

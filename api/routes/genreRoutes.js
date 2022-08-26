5208bafe21e8efdd4ca014de1b0a31e9975143da
emanuelretamozo <eretamozov@gmail.com>
2022-08-25 19:34:00 -0500

    add: fixing conflicts

--- api/routes/genreRoutes.js
+++ api/routes/genreRoutes.js
@@ -5,15 +5,13 @@ import updateGenreValidator from '../middlewares/updateGenreValidator.js';
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.route('/genres').post(createGenreValidator, genreController.createGenre);
-router.route('/genres/:id').delete(genreController.deleteGenreById);
-=======
-router.route('/genres')
-  .post(createGenreValidator, genreController.createGenre);
->>>>>>> c2a847a91712f8ee01adcc00b96e9cf88c2d5251
+router
+  .route('/genres')
+    .post(createGenreValidator, genreController.createGenre);
 
-router.route('/genres/:id')
-  .put(updateGenreValidator, genreController.updateGenreById);
+router
+  .route('/genres/:id')
+    .delete(genreController.deleteGenreById)
+    .put(updateGenreValidator, genreController.updateGenreById);
 
 export default router;
